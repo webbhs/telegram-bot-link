e4198967b2ae1c35ec4352503d722fb8f3f3f25f
webbhs <75515918+webbhs@users.noreply.github.com>
2021-07-11 22:32:02 +0530

    Update link.py

--- link.py
+++ link.py
@@ -69,7 +69,7 @@ def find(r):
 
   url=f"https://paste.ee/p/{id}"
 
-  response = requests.put("https://api.shorte.st/v1/data/url", {"urlToShorten":url}, headers={"public-api-token": "6ecac39ce3355cfdec48dea0220b6d4e")
+  response = requests.put("https://api.shorte.st/v1/data/url", {"urlToShorten":url}, headers={"public-api-token": "6ecac39ce3355cfdec48dea0220b6d4e"})
   print(response.content)
   # {"status":"ok","shortenedUrl":"http:\\/\\/sh.st\\/ryHyU"}
 
